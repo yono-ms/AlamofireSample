0d2756b3e1a8721bc46df12133a86d0f3cc08396
yono.ap <yono.ap@icloud.com>
2024-02-09 20:32:01 +0900

    Initial Commit

---
+++ AlamofireSample/AlamofireSampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  AlamofireSampleApp.swift
+//  AlamofireSample
+//
+//  Created by no name on 2024/02/09.
+//  
+//
+
+import SwiftUI
+
+@main
+struct AlamofireSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlamofireSample/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  AlamofireSample
+//
+//  Created by no name on 2024/02/09.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
